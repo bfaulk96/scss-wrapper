892c0607fe013f13a951eea68b4d4da43d39a003
Brandon Faulkner <college.bfaulk96@yahoo.com>
2018-06-06 08:45:25 -0500

    Added the file.

---
+++ scss-wrapper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "No argument provided. Using folder name."
+  folderName=${PWD##*/}
+  if [[ $folderName == *" "* ]]; then
+    folderName=${folderName// /"-"}
+    echo "Converting spaces to hyphens in project name."
+  fi
+else
+  folderName="$1"
+fi
+if [[ $folderName =~ ^[A-Za-z0-9_-]+$ ]]; then
+  echo "Project name is $folderName."
+  prefix=".$folderName {"
+  suffix="}"
+
+  for file in $(find . -regex ".*/*.scss"); do
+    if [[ $file != *"node_modules"* ]] &&
+    [[ $file == *"src"* ]] &&
+    ! grep -q "$prefix" "$file" &&
+    ! grep -q "@import" "$file"; then
+      sed -i '' 's/^/\\t/' "$file" &&
+      echo -e "$prefix\n$(cat $file)\n$suffix" > "$file"
+      echo "Wrapper has been added to $file"
+    else
+      if [[ $file == *"node_modules"* ]]; then
+          continue
+      else
+        if grep -q "@import" "$file"; then
+          echo "@import does not support scss nesting. Please move @import statements to styles.scss file. Skipping $file"
+        else
+          if [[ $file != *"src"* ]]; then
+            echo "$file is not in src directory. Skipped"
+          else
+            echo "Wrapper already exists in $file; Skipping"
+          fi
+        fi
+      fi
+    fi
+  done
+else
+  echo "Project name must contain only alphanumeric characters, underscore, and hyphen."
+fi
